6a1553768c1870a6a81080a5cbd20339fb611691
Seiji Naganuma <snaganumas@gmail.com>
2015-08-01 16:02:09 -0400

    add cdn and remove browserify backbone and merionette

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require_tree ../../../vendor/assets/javascripts/.
-//= require_tree .
+
 Backbone.Syphon = require('backbone.syphon');
 
 var LatencyApp = require('latency/application.js');
